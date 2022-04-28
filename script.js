a6d6e70b74a9c23ed71e28a8ba9fbe3f2eab5385
juan Carlos <juancarlosm.nunez@gmail.com>
2022-04-28 16:18:05 -0300

    set characters life

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
     setTimeout(function(){
         character.classList.remove("animate")
     },500)  
+
 }
+var Life = setInterval(function(){
+    var characterTop = 
+    ParseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft= 
+    ParseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 %% characterTop>=130){
+        block.style.animation = "none";
+        block.style.display= "none";
+        alert("crash!!");
+    }
+},10);
+
